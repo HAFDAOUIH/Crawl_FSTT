191bc9d34760608ab97f4dde4ad1a7359c407073
hafdaoui <lilpump0629@gmail.com>
2024-04-19 13:46:34 +0100

    1

---
+++ FSTT_spider/FSTT_spider/spiders/FORMATION_INITIALE.py
@@ -0,0 +1,9 @@
+import scrapy
+
+
+class FormationInitialeSpider(scrapy.Spider):
+    name = "FORMATION-INITIALE"
+    start_urls = ["https://fstt.ac.ma/Portail2023/formation-initiale/"]
+
+    def parse(self, response):
+        response.css('div.elementor-text-editor').xpath('string()').get().strip()
