adfa910ee8a615d2c55c81e33df4231cc154e913
sasamg <salma.amgarou@etu.uae.ac.ma>
2024-04-23 15:31:37 +0100

    1531

--- FSTT_spider/FSTT_spider/spiders/equipes_recherche.py
+++ FSTT_spider/FSTT_spider/spiders/equipes_recherche.py
@@ -1,18 +1,25 @@
 import scrapy
 
 
-class RechercheStructSpider(scrapy.Spider):
-    name = "recherche_struct"
+class EquipesRechercheSpider(scrapy.Spider):
+    name = "equipes_recherche"
     allowed_domains = ["fstt.ac.ma"]
-    start_urls = ["https://fstt.ac.ma/Portail2023/laboratoires-de-recherche/"]
+    start_urls = ["https://fstt.ac.ma/Portail2023/equipes-de-recherche/"]
 
     def parse(self, response):
         title = response.css('h2.elementor-heading-title::text').extract_first()
-        links = response.css(
+        div_links = response.css(
             'div.has_eae_slider.elementor-column.elementor-col-100.elementor-top-column.elementor-element::attr(data-wts-url)').getall()
-
-        for link in links:
+        for link in div_links:
             yield response.follow(link, callback=self.parse_content, meta={'title': title})
+
+        # Extract links from a tag if present
+        for link in div_links:
+            a_tags = response.css(
+                f'div.has_eae_slider.elementor-column.elementor-col-100.elementor-top-column.elementor-element[data-wts-url="{link}"] a::attr(href)').getall()
+            for a_tag in a_tags:
+                yield response.follow(a_tag, callback=self.parse_content, meta={'title': title})
+
 
     def parse_content(self, response):
         title = response.meta['title']
@@ -34,8 +41,6 @@ class RechercheStructSpider(scrapy.Spider):
 
         # Extract these et habilitations soutenues
         these_habil_soutenues_link = response.css('div#elementor-tab-content-1675 a::attr(href)').get()
-
-
 
         yield {
             'Title': title,
